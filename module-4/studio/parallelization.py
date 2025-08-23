7e611146866ad700d2f78805cbece47a2bee95c8
Thomas Walker <thwalker@vt.edu>
2025-08-23 15:10:08 -0400

    module 4 updates

--- module-4/studio/parallelization.py
+++ module-4/studio/parallelization.py
@@ -12,7 +12,7 @@ from langchain_openai import ChatOpenAI
 
 from langgraph.graph import StateGraph, START, END
 
-llm = ChatOpenAI(model="gpt-4o", temperature=0) 
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0) 
 
 class State(TypedDict):
     question: str
